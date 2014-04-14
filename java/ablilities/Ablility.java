94ccf93d1322362fdd3cc028029bc80a57c42f6d
sonicdeadlock <soncideadlock369@gmail.com>
2014-04-14 04:41:52 -0400

    Added abstract class ablility

---
+++ java/ablilities/Ablility.java
@@ -0,0 +1,31 @@
+package ablilities;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Ablility.
+ */
+public abstract class Ablility {
+	
+	/** The level. */
+	private int level=0;
+	
+	/** The name. */
+	private String name="";
+
+	/**
+	 * Instantiates a new ablility.
+	 */
+	public Ablility() {
+		
+	}
+	
+	/**
+	 * Gets the type.
+	 *
+	 * @return the type
+	 */
+	abstract String getType();
+	
+	
+
+}
