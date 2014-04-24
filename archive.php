1267c0aea48bdb1ebc89045c1b29561a58155c6b
sonicdeadlock <sonicdeadlock369@gmail.com>
2014-04-24 18:17:19 -0400

    Improvements to archive
    -Entries per page customizable
    -Integrated new Permissions system

--- archive.php
+++ archive.php
@@ -1,27 +1,37 @@
 <?php
 require_once 'core/init.php';
 $user = new User();
-$per_page=20;
-if($user->isAdmin()){
+$per_page;
+if($user->hasPermission('archive')){
+	if(Input::get('per_page')!=""){
+		Cookie::put("per_page",Input::get('per_page'),3600*24*500);
+		$per_page = Input::get('per_page');
+	}else if(Cookie::exists('per_page')){
+		$per_page = Cookie::get('per_page');
+	}
+	else{
+		$per_page=20;
+	}
 	$db=DB::getInstance();
 	$num_chats = $db->tableCount('chat');
 	$page = (Input::get('page')!="")?Input::get('page'):1;
 	$pages = ceil($num_chats/$per_page);
 	if($page>$pages || $page<=0){
-		echo 'Invalid Page Number';
+		Redirect::to('archive.php');
 	}
 	else
-	{$chats = $db->getAll('chat',null,$per_page*($page-1)+1,$per_page*$page)->results();
-		echo $per_page*($page-1) . "   " .  $per_page*$page ;
+	{$chats = $db->getAll('chat',null,$per_page*($page-1)+1,$per_page)->results();
 		echo "<ol>";
 		foreach ($chats as $key => $chat) {
 			$username = $chat->username;
 	                 		$color=$chat->name_color;
 	                 		$t = $chat->message;
 	                 		$time=$chat->time;
+	                 		if($color == 'white')
+	                 			$color = "333";
 	                 		echo  '<li>'.
 	                 		'<span class="time">' . $time . "</span> " .
-	                 		'<a class="username"  href="profile.php?username=' . $username . '" target="_blank">' . $username . "</a>: " .
+	                 		'<a class="username"  href="profile.php?username=' . $username . '" target="_blank" style="color:' . $color . '">' . $username . "</a>: " .
 	                 		$t . "</li>";
 					
 			}}
@@ -29,7 +39,15 @@ if($user->isAdmin()){
 			if($page>1){
 				echo "<a href='archive.php?page=" . ($page-1) . "'> prev </a>";
 			}
-			for ($i=1; $i <=$pages ; $i++) { 
+			$min=1;
+			$max=$pages;
+			if($page>5){
+				$min=$page-5;
+			}
+			if($pages-$page>5){
+				$max=$page+5;
+			}
+			for ($i=$min; $i <=$max ; $i++) { 
 				if($i==$page){
 					echo "<b> " . $i . "</b>"; 
 				}
@@ -39,6 +57,12 @@ if($user->isAdmin()){
 			}
 			if($page<$pages){
 				echo "<a href='archive.php?page=" . ($page+1) . "'> next </a>";
+			}
+			echo '<br>';
+			echo "<p>Per Page</p>";
+			$values = array(20,50,100);
+			foreach ($values as $key => $value) {
+				echo "<a href='archive.php?page={$page}&per_page={$value}'> {$value}</a>";
 			}
 
 }
